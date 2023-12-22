bf56e086503d9a0dbe03d1a21f2a51dcc3b62cb1
Shreejal Maharjan <shreejal27@gmail.com>
2023-12-22 17:31:49 +0545

    customized top bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,36 @@ import React from "react";
 
 import "./home.css";
 
-const Home = () =>{
-    return(
+const Home = () => {
+    return (
         <div>
-        <div className="spotifyHome">
-            <p>This is from Home</p>
-        </div>
-              <div className="spotifyPlaylists">      
-              <h1>This is from Home</h1>
+            <div className="spotifyHome">
+                <div className="spotifyHomeTop">
+                    <div className="spotifyHomeTopLeft">
+                        <div>
+                            <i class="fa-solid fa-circle-chevron-left fa-2xl" style={{ color: "#000000" }}></i>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-circle-chevron-right fa-2xl" style={{ color: "#000000" }}></i>
+                        </div>
+                    </div>
+                    <div className="spotifyHomeTopRight">
+                        <div className="circle">
+                            <i class="fa-solid fa-circle fa-2xl" style={{ color: "#272727", position: "relative" }}></i>
+                            <i class="fa-solid fa-users fa-sm" style={{ color: "white", position: "absolute" }}></i>
+                        </div>
+                        <div className="circle">
+                            <i class="fa-solid fa-circle fa-2xl" style={{ color: "#272727", position: "relative" }}></i>
+                            <i class="fa-solid fa-users fa-sm" style={{ color: "white", position: "absolute" }}></i>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p className="greetings">Good afternoon</p>
+                </div>
+            </div>
+            <div className="spotifyPlaylists">
+                <h1>Made For Rajwi</h1>
             </div>
         </div>
     )
