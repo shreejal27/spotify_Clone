f23934dffb05aaf765a02c4e7ceb55d0cf98bdf8
Shreejal Maharjan <shreejal27@gmail.com>
2023-12-24 19:54:20 +0545

    added search input

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,33 @@
 import React from "react";
-
-const Search = () =>{
-    return(
+import "./search.css";
+const Search = () => {
+    return (
         <div>
-            <h1>This is from Search</h1>
+            <div className="spotifySearch">
+                <div className="spotifyHomeTop">
+                    <div className="spotifyHomeTopLeft">
+                        <div>
+                            <i class="fa-solid fa-circle-chevron-left fa-2xl" style={{ color: "#000000" }}></i>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-circle-chevron-right fa-2xl" style={{ color: "#000000" }}></i>
+                        </div>
+                    </div>
+                    <div className="spotifySearchInput">       
+                            <input type="text" className="spotifySearchForm" placeholder="What do you want to listen to?"/>
+                    </div>
+                    <div className="spotifyHomeTopRight">
+                        <div className="circle">
+                            <i class="fa-solid fa-circle fa-2xl" style={{ color: "#272727", position: "relative" }}></i>
+                            <i class="fa-solid fa-users fa-sm" style={{ color: "white", position: "absolute" }}></i>
+                        </div>
+                        <div className="circle">
+                            <i class="fa-solid fa-circle fa-2xl" style={{ color: "#272727", position: "relative" }}></i>
+                            <i class="fa-solid fa-users fa-sm" style={{ color: "white", position: "absolute" }}></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
