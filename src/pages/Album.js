6dc4588b122e13f731222ff742e93aa24379f8cf
Shreejal Maharjan <shreejal27@gmail.com>
2024-01-04 19:07:10 +0545

    css for album page

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,9 +1,46 @@
 import React from "react";
 
-const Album = () =>{
-    return(
-        <div className="albumn">
-            <h1>This is from Album</h1>
+import "./album.css";
+
+const Album = () => {
+    return (
+        <div>
+            <div className="spotifyHome">
+                <div className="spotifyHomeTop">
+                    <div className="spotifyHomeTopLeft">
+                        <div>
+                            <i class="fa-solid fa-circle-chevron-left fa-2xl" style={{ color: "#000000" }}></i>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-circle-chevron-right fa-2xl" style={{ color: "#000000" }}></i>
+                        </div>
+                    </div>
+                    <div className="spotifyHomeTopRight">
+                        <div className="circle">
+                            <i class="fa-solid fa-circle fa-2xl" style={{ color: "#272727", position: "relative" }}></i>
+                            <i class="fa-solid fa-users fa-sm" style={{ color: "white", position: "absolute" }}></i>
+                        </div>
+                        <div className="circle">
+                            <i class="fa-solid fa-circle fa-2xl" style={{ color: "#272727", position: "relative" }}></i>
+                            <i class="fa-solid fa-users fa-sm" style={{ color: "white", position: "absolute" }}></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div className = "albumHero">
+                    <div className="albumHeroImage">
+                        a
+                    </div>
+                    <div className="albumHeroText">
+                        <div className= "albumHeroTextMargin">
+                        <p className="playlistType">Public Playlist</p>
+                        <p className="playlistName">Juice Wrld</p>
+                        <p className="playlistText">ululululul</p>
+                        <p className="playlistDetails">Rajwi 43 songs, 2 hr 14 min</p>
+                     </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
